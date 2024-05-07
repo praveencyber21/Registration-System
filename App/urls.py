2ec5183299245de7a04a1630f69d21dc7d55e18b
praveencyber21 <praveen.m.cyber@gmail.com>
2024-05-07 18:48:38 +0530

    feat: Config urls and views

--- App/urls.py
+++ App/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from App import views
 
 urlpatterns = [
-    path()
+    path('', views.login, name='login'),
+    path('signup', views.signup, name='signup')
 ]
--- App/views.py
+++ App/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.template import loader
 # Create your views here.
+
+def home(request):
+    pass
+
+def login(request):
+    template = loader.get_template('login.html')
+    return HttpResponse(template.render())
+
+def signup(request):
+    template = loader.get_template('signup.html')
+    return HttpResponse(template.render())
+
